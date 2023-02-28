08354fe48eed8225977665ee727480309ee9b39b
Ruth Jaquelina Cruz <jaquelinacruz.95@gmail.com>
2023-02-28 20:53:04 -0300

    En este commit se encuentra el CV generado como proyecto integrador del curso Yo Programo 4.0

---
+++ js/funcionalidad.js
@@ -0,0 +1,29 @@
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+const coleccion=document.getElementsByClassName('titulo3');
+for(let i =0;i<coleccion.length ;i++){
+    coleccion[i].addEventListener('mouseover', function(){
+        coleccion[i].style.color='red';
+    });
+};
+for(let i =0;i<coleccion.length ;i++){
+    coleccion[i].addEventListener('mouseout', function(){
+        coleccion[i].style.color='orange';
+    });
+};
+
+document.querySelector('html').ondblclick = function() {
+    alert('¡Ouch! ¡Deja de pincharme!');
+}
+
+function alerta()
+    {
+    var mensaje= "Gracias por su atención, espero formar parte del equipo de la empresa" ;
+	document.getElementById("ejemplo").innerHTML = mensaje;
+}
+
